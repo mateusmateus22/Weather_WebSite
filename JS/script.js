032bff00141f68cc0adfb7afeb9ed9a716a6ea52
Orlando Neto <orlandinho.nerd@gmail.com>
2023-06-26 23:17:19 -0300

    Atualizando

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
-const key = "d2b39b1ca7a292206e092ce5ee116501"
+const key = "d2b39b1ca7a292206e092ce5ee116501";
 
-function viweData(dados){
+function viweData(dados) {
+
     document.querySelector(".city").innerHTML = "Tempo em " + dados.name
     document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
     document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
@@ -8,10 +9,10 @@ function viweData(dados){
     document.querySelector(".img-previsao").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png" 
 }
 
-async function buscarCidade(cidade){ //utilizar async para alertar o código que iremos acessar um servidor
+async function buscarCidade(cidade) { //utilizar async para alertar o código que iremos acessar um servidor
 
     const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json()).catch(Error => error) /*await serve para que o JS espere até que o servidor responda &&  /fetch() serve para acessarmos o servidor e as crases no inicio e fim permite que eu coloque uma variavel no meio do endereço da key */
-                          
+    
     changeBG(dados.weather[0].id);
     
     console.log(dados)
